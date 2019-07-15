92a39fc69831d5644912bab411f173785a96e652
Melvin salcedo <melvinsalcedo169@gmail.com>
2019-07-15 12:28:21 -0500

    .

---
+++ qt_Molecular/Dbscan.py
@@ -0,0 +1,165 @@
+from UI_molecular import *
+import numpy as np
+from matplotlib import pyplot as plt
+import cv2
+import time
+from scipy.spatial.distance import cdist
+from sklearn.cluster import KMeans
+
+from sklearn.cluster import DBSCAN
+import kmedoids
+from sklearn.metrics.pairwise import pairwise_distances
+
+
+class Clustering():
+	def __init__(self, img, image):
+		
+		self.img = img
+		self.image = image
+
+	def K_Means(self):
+
+		km = KMeans(n_clusters=4,max_iter=300)
+		km.fit(self.image)
+
+		centers = km.cluster_centers_
+		
+		print(centers);
+		print("__________________________");
+		point_distances = cdist(centers, self.image, 'euclidean')
+		cluster_indexes = np.argmin(point_distances, axis=0)
+		segmented = centers[cluster_indexes]
+		print(cluster_indexes);
+		segmented_image = segmented.reshape(self.img.shape).astype(np.uint8)
+
+		return segmented_image
+
+	def DB_SCAN(self):
+		db = DBSCAN(eps=255*0.01, min_samples=20, metric='euclidean')
+		db.fit(self.image)
+
+		number_of_clusters = np.max(db.labels_) + 1
+		#print("Number of clusters : ", number_of_clusters)
+
+		centers = np.zeros((number_of_clusters, 3))
+		for i in range(0, number_of_clusters):
+		    cluster_points = self.image[db.labels_ == i]
+		    cluster_mean = np.mean(cluster_points, axis=0)
+		    centers[i, :] = cluster_mean
+
+		point_distances = cdist(centers, self.image, 'euclidean')
+		cluster_indexes = np.argmin(point_distances, axis=0)
+
+		segmented = centers[cluster_indexes]
+	
+		segmented_image = segmented.reshape(self.img.shape).astype(np.uint8)
+		return segmented_image
+
+	def K_MEDOIDS(self):
+		data = np.array([[1,1], 
+        	        [2,2], 
+    	            [10,10]])
+
+		D = pairwise_distances(self.data, metric='euclidean')
+		# split into 2 clusters
+		M, C = kmedoids.kMedoids(D, 2)
+
+		#print('medoids:')
+		for point_idx in M:
+		    print( data[point_idx] )
+
+		#print('')
+		#print('clustering result:')
+		for label in C:
+		    for point_idx in C[label]:
+		        print('label {0}:　{1}'.format(label, data[point_idx]))
+		return D;
+    
+    
+class MainWindow(QtWidgets.QMainWindow, Ui_MainWindow):
+    def __init__(self, *args, **kwargs):       
+        QtWidgets.QMainWindow.__init__(self, *args, **kwargs)
+        self.setupUi(self)  
+        self.ImagenEntrada.setText("Haz clic en el botón")
+        #self.OpenImage.setText("Presióname")
+        
+        self.OpenImage.clicked.connect(self.OpemImage)
+        
+        self.Qmeans.clicked.connect(self.Kmeans)
+        self.DbScan.clicked.connect(self.DBscan)
+            
+        
+    def actualizar(self):
+        self.ImagenEntrada.setText("¡Acabas de hacer clic en el botón!")
+        
+    
+    def OpemImage(self):
+        # Create widget
+        label = QLabel(self)
+        pixmap = QPixmap('entrada.jpg')
+        self.ImagenEntrada.setPixmap(pixmap)
+
+    def Kmeans(self):
+        start = time.time()
+        img = cv2.imread("entrada.jpg",1)
+        image = img.reshape(-1, img.shape[-1])
+        cluster = Clustering(img,image)
+        segmented_image = cluster.K_Means()
+        cv2.imwrite('KMeans.jpg',segmented_image)
+
+        
+        label = QLabel(self)
+        pixmap = QPixmap('KMeans.jpg')
+        self.EimagenSalida.setPixmap(pixmap)
+        end = time.time()
+        print(end - start)
+    
+    def DBscan(self):
+        img = cv2.imread("entrada1.jpg",1)
+        image = img.reshape(-1, img.shape[-1])
+        cluster = Clustering(img,image)
+        segmented_image = cluster.DB_SCAN()
+        
+        img2 = cv2.resize(segmented_image,(256,256))
+        cv2.imwrite('DBSCAN.jpg',img2)
+
+       
+        
+        label = QLabel(self)
+        pixmap = QPixmap('DBSCAN.jpg')
+        self.EimagenSalida.setPixmap(pixmap)
+        
+        
+if __name__ == "__main__":
+    app = QtWidgets.QApplication([])
+    window = MainWindow()
+    window.show()
+    app.exec_()
+    
+    
+      
+    
+"""    
+if __name__ == "__main__":
+	img = cv2.imread("entrada.jpg",1)
+	print("Image shape : ",img.shape)
+	
+	image = img.reshape(-1, img.shape[-1])
+	cluster = Clustering(img, image)
+
+	print("KMEANS CLUSTERING")
+	segmented_image = cluster.K_Means()
+	cv2.imwrite('KMeans.jpg',segmented_image)
+    cv2.imshow('Kmeans', segmented_image)
+
+	#print("DBSCAN CLUSTERING")
+	#segmented_image = cluster.DB_SCAN()
+	#cv2.imwrite('DBSCAN.jpg',segmented_image)
+	#print(image)
+	#print("k-medois CLUSTERING")
+	
+
+	#segmented_image=cluster.K_MEDOIDS()
+	#segmented_image = cluster.K_MEDOIDS()
+	#cv2.imwrite('k-medois.jpg',segmented_image)
+"""
---
+++ qt_Molecular/UI_molecular.py
@@ -0,0 +1,69 @@
+# -*- coding: utf-8 -*-
+
+# Form implementation generated from reading ui file 'UI_molecular.ui'
+#
+# Created by: PyQt5 UI code generator 5.9.2
+#
+# WARNING! All changes made in this file will be lost!
+
+from PyQt5 import QtCore, QtGui, QtWidgets
+from PyQt5.QtWidgets import QApplication, QWidget, QLabel
+from PyQt5.QtGui import QIcon, QPixmap
+
+class Ui_MainWindow(object):
+    def setupUi(self, MainWindow):
+        MainWindow.setObjectName("MainWindow")
+        MainWindow.resize(800, 600)
+        self.centralwidget = QtWidgets.QWidget(MainWindow)
+        self.centralwidget.setObjectName("centralwidget")
+        self.ImagenEntrada = QtWidgets.QLabel(self.centralwidget)
+        self.ImagenEntrada.setGeometry(QtCore.QRect(10, 190, 351, 341))
+        self.ImagenEntrada.setText("")
+        self.ImagenEntrada.setObjectName("ImagenEntrada")
+        self.EimagenSalida = QtWidgets.QLabel(self.centralwidget)
+        self.EimagenSalida.setGeometry(QtCore.QRect(420, 200, 351, 341))
+        self.EimagenSalida.setText("")
+        self.EimagenSalida.setObjectName("EimagenSalida")
+        self.OpenImage = QtWidgets.QPushButton(self.centralwidget)
+        self.OpenImage.setGeometry(QtCore.QRect(20, 70, 93, 28))
+        self.OpenImage.setObjectName("OpenImage")
+        self.splitter = QtWidgets.QSplitter(self.centralwidget)
+        self.splitter.setGeometry(QtCore.QRect(470, 60, 279, 28))
+        self.splitter.setOrientation(QtCore.Qt.Horizontal)
+        self.splitter.setObjectName("splitter")
+        self.DbScan = QtWidgets.QPushButton(self.splitter)
+        self.DbScan.setObjectName("DbScan")
+        self.Qmeans = QtWidgets.QPushButton(self.splitter)
+        self.Qmeans.setObjectName("Qmeans")
+        self.Qmedois = QtWidgets.QPushButton(self.splitter)
+        self.Qmedois.setObjectName("Qmedois")
+        MainWindow.setCentralWidget(self.centralwidget)
+        self.menubar = QtWidgets.QMenuBar(MainWindow)
+        self.menubar.setGeometry(QtCore.QRect(0, 0, 800, 26))
+        self.menubar.setObjectName("menubar")
+        MainWindow.setMenuBar(self.menubar)
+        self.statusbar = QtWidgets.QStatusBar(MainWindow)
+        self.statusbar.setObjectName("statusbar")
+        MainWindow.setStatusBar(self.statusbar)
+
+        self.retranslateUi(MainWindow)
+        QtCore.QMetaObject.connectSlotsByName(MainWindow)
+
+    def retranslateUi(self, MainWindow):
+        _translate = QtCore.QCoreApplication.translate
+        MainWindow.setWindowTitle(_translate("MainWindow", "MainWindow"))
+        self.OpenImage.setText(_translate("MainWindow", "Open Image"))
+        self.DbScan.setText(_translate("MainWindow", "DbScan"))
+        self.Qmeans.setText(_translate("MainWindow", "Qmeans"))
+        self.Qmedois.setText(_translate("MainWindow", "Qmedoids"))
+
+
+if __name__ == "__main__":
+    import sys
+    app = QtWidgets.QApplication(sys.argv)
+    MainWindow = QtWidgets.QMainWindow()
+    ui = Ui_MainWindow()
+    ui.setupUi(MainWindow)
+    MainWindow.show()
+    sys.exit(app.exec_())
+
