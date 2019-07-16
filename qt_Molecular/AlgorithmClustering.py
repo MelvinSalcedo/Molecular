64f8ec0d29f0295748c534365534403f07793250
Melvin salcedo <melvinsalcedo169@gmail.com>
2019-07-15 22:49:42 -0500

    all proyect

--- qt_Molecular/AlgorithmClustering.py
+++ qt_Molecular/AlgorithmClustering.py
@@ -9,53 +9,38 @@ from kmedoids_image import *
 from sklearn.cluster import DBSCAN
 from sklearn.metrics.pairwise import pairwise_distances
 
-
 class Clustering():
-	def __init__(self, img, image):
-		
-		self.img = img
-		self.image = image
-
-	def K_Means(self):
-
-		km = KMeans(n_clusters=4,max_iter=300)
-		km.fit(self.image)
-
-		centers = km.cluster_centers_
-		
-		print(centers);
-		print("__________________________");
-		point_distances = cdist(centers, self.image, 'euclidean')
-		cluster_indexes = np.argmin(point_distances, axis=0)
-		segmented = centers[cluster_indexes]
-		print(cluster_indexes);
-		segmented_image = segmented.reshape(self.img.shape).astype(np.uint8)
-
-		return segmented_image
-
-	def DB_SCAN(self):
-		db = DBSCAN(eps=255*0.01, min_samples=20, metric='euclidean')
-		db.fit(self.image)
-
-		number_of_clusters = np.max(db.labels_) + 1
-		#print("Number of clusters : ", number_of_clusters)
-
-		centers = np.zeros((number_of_clusters, 3))
-		for i in range(0, number_of_clusters):
-		    cluster_points = self.image[db.labels_ == i]
-		    cluster_mean = np.mean(cluster_points, axis=0)
-		    centers[i, :] = cluster_mean
- 
-		point_distances = cdist(centers, self.image, 'euclidean')
-		cluster_indexes = np.argmin(point_distances, axis=0)
-
-		segmented = centers[cluster_indexes]
-	
-		segmented_image = segmented.reshape(self.img.shape).astype(np.uint8)
-		return segmented_image
+    def __init__(self, img, image):
+        self.img = img
+        self.image = image
+        
+    def K_Means(self):
+        km = KMeans(n_clusters=4,max_iter=300)
+        km.fit(self.image)
+        centers = km.cluster_centers_
+        point_distances = cdist(centers, self.image, 'euclidean')
+        cluster_indexes = np.argmin(point_distances, axis=0)
+        segmented = centers[cluster_indexes]
+        segmented_image = segmented.reshape(self.img.shape).astype(np.uint8)
+        return segmented_image
+    def DB_SCAN(self):
+        db = DBSCAN(eps=255*0.01, min_samples=20, metric='euclidean')
+        db.fit(self.image)
+        number_of_clusters = np.max(db.labels_) + 1
+        centers = np.zeros((number_of_clusters, 3))
+        for i in range(0, number_of_clusters):
+            cluster_points = self.image[db.labels_ == i]
+            cluster_mean = np.mean(cluster_points, axis=0)
+            centers[i, :] = cluster_mean
+        point_distances = cdist(centers, self.image, 'euclidean')
+        cluster_indexes = np.argmin(point_distances, axis=0)
+        segmented = centers[cluster_indexes]
+        segmented_image = segmented.reshape(self.img.shape).astype(np.uint8)
+        return segmented_image
     
-        def K_MEDOIDS(self):
-            LLamar()
+    def K_MEDOIDS(self):
+        LLamar()
+        
 class MainWindow(QtWidgets.QMainWindow, Ui_MainWindow):
     def __init__(self, *args, **kwargs):       
         QtWidgets.QMainWindow.__init__(self, *args, **kwargs)
@@ -110,15 +95,19 @@ class MainWindow(QtWidgets.QMainWindow, Ui_MainWindow):
         self.EimagenSalida.setPixmap(pixmap)
         
     def Kmedoids(self):
+        img = cv2.imread("Kmedios.jpg",1)
+        image = img.reshape(-1, img.shape[-1])
+        cluster = Clustering(img,image)
         segmented_image = cluster.K_MEDOIDS()
         
-        img2 = cv2.resize(segmented_image,(256,256))
-        cv2.imwrite('DBSCAN.jpg',img2)
+        img__ = cv2.imread("Kmedios.jpg",1)
+        img2 = cv2.resize(img__,(256,256))
+        cv2.imwrite('KmediosResize.jpg',img2)
 
        
         
         label = QLabel(self)
-        pixmap = QPixmap('DBSCAN.jpg')
+        pixmap = QPixmap('KmediosResize.jpg')
         self.EimagenSalida.setPixmap(pixmap)
         
 if __name__ == "__main__":
@@ -126,31 +115,3 @@ if __name__ == "__main__":
     window = MainWindow()
     window.show()
     app.exec_()
-    
-    
-      
-    
-"""    
-if __name__ == "__main__":
-	img = cv2.imread("entrada.jpg",1)
-	print("Image shape : ",img.shape)
-	
-	image = img.reshape(-1, img.shape[-1])
-	cluster = Clustering(img, image)
-
-	print("KMEANS CLUSTERING")
-	segmented_image = cluster.K_Means()
-	cv2.imwrite('KMeans.jpg',segmented_image)
-    cv2.imshow('Kmeans', segmented_image)
-
-	#print("DBSCAN CLUSTERING")
-	#segmented_image = cluster.DB_SCAN()
-	#cv2.imwrite('DBSCAN.jpg',segmented_image)
-	#print(image)
-	#print("k-medois CLUSTERING")
-	
-
-	#segmented_image=cluster.K_MEDOIDS()
-	#segmented_image = cluster.K_MEDOIDS()
-	#cv2.imwrite('k-medois.jpg',segmented_image)
-"""
---
+++ qt_Molecular/kmedoids_image.py
@@ -0,0 +1,116 @@
+import csv
+from numpy import *
+from PIL import Image
+from scipy.spatial.distance import pdist
+
+def loadimage(fname):
+    fp = open(fname, "rb")
+    ImageSet=[]
+    im = Image.open(fp)
+    m,n = im.size
+    for i in range(m):
+        for j in range(n):
+            tmp = []
+            x, y, z = im.getpixel((i, j))
+            tmp.append(x / 256.0)
+            tmp.append(y / 256.0)
+            tmp.append(z / 256.0)
+            ImageSet.append(tmp)
+    fp.close()
+    return ImageSet,m,n
+
+def pearson_distance(vector1, vector2):
+    X = vstack([vector1, vector2])
+    d2 = pdist(X)
+    return d2
+
+distances_cache = {}
+
+def totalcost(dataset, medoids_idx):
+    size = len(dataset)
+    total_cost = 0.0
+    medoids = {}
+    
+    for idx in medoids_idx:
+        medoids[idx] = []
+    for i in range(size):
+        choice = None
+        min_cost = inf
+        for m in medoids:
+            tmp = distances_cache.get((m, i), None)
+            if tmp is None:
+                tmp = pearson_distance(dataset[m], dataset[i])
+                distances_cache[(m, i)] = tmp
+            if tmp < min_cost:
+                choice = m
+                min_cost = tmp
+        medoids[choice].append(i)
+        total_cost += min_cost
+    #print(distances_cache)
+    return total_cost, medoids
+
+def kmedoids(dataset, k):
+    size = len(dataset)
+    medoids_idx = random.choice(size,k,replace=False)
+    medoids_idx = medoids_idx.tolist()
+    print(medoids_idx)
+    pre_cost, medoids = totalcost(dataset, medoids_idx)
+    print(pre_cost)
+    
+    current_cost = inf
+    best_medoids_idx = []
+    best_medoids = {}
+    iter_count = 0
+    int_y=0
+    while 1:
+        int_y+=1
+        print(int_y)
+        if(int_y==10):
+            break
+        for m in medoids:
+            for item in medoids[m]:
+                if item != m:
+                    idx = medoids_idx.index(m)
+                    swap_temp = medoids_idx[idx]
+                    medoids_idx[idx] = item
+                    tmp, medoids_ = totalcost(dataset, medoids_idx)
+                    if tmp < current_cost:
+                        best_medoids_idx = list(medoids_idx)
+                        best_medoids = dict(medoids_)
+                        current_cost = tmp
+                    medoids_idx[idx] = swap_temp
+        iter_count += 1
+        #print(current_cost)
+        #print(iter_count)
+        if best_medoids_idx == medoids_idx: break
+        if current_cost <= pre_cost:
+            pre_cost = current_cost
+            medoids = best_medoids
+            medoids_idx = best_medoids_idx
+
+    return current_cost, best_medoids_idx, best_medoids
+
+
+def LLamar():
+    ImageSet,m,n=loadimage(r"entrada1.jpg")
+    best_cost, best_medoids_idx, best_medoids=kmedoids(ImageSet,4)
+    print(best_cost)
+    print(best_medoids_idx)
+    print(best_medoids)
+    medoids_new={}
+    for m1 in best_medoids_idx:
+        medoids_new[m1]=[]
+        for y in range(len(ImageSet[m1])):
+            temp=int(ImageSet[m1][y]*256)
+            medoids_new[m1].append(temp)
+
+    pic_new = Image.new("RGB", (m, n)) 
+    for m2 in best_medoids:
+            for item in best_medoids[m2]:
+                pic_new.putpixel((int(item/n),int(item%n)),tuple((medoids_new[m2])))
+
+    pic_new.save("Kmedios.jpg","JPEG")
+    
+
+
+
